6602c48a8918be66d06707b6028c85b2a8808f70
Mats Lindblad <mats.lindblad@gmail.com>
2020-10-29 19:35:50 +0100

    Updates

---
+++ precache-manifest.0a8c7319dc6be1eb70dbc823ccac12f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2a8237c3ecfc55bd52847b31e93775",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b8b91ff5fd434fc843bd",
+    "url": "./static/css/main.56beedbb.chunk.css"
+  },
+  {
+    "revision": "0136089d572c4e2c75f0",
+    "url": "./static/js/2.c1eefab2.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.c1eefab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b91ff5fd434fc843bd",
+    "url": "./static/js/main.6a9aa6a5.chunk.js"
+  },
+  {
+    "revision": "b84f22630f0d348e166c",
+    "url": "./static/js/runtime-main.7eafff6d.js"
+  }
+]);
